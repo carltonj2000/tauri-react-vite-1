13d7d84ab6a26ef44aba3cc8d42bad1acf25652b
Carlton Joseph <carlton.joseph@gmail.com>
2022-03-27 16:57:39 -0700

    add local storage

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Tauri, React and Vite
 
-The code in this repository is base on the following:
+Install, run and debug or build and run via the commands below.
 
-- [Tauri & ReactJS Part 5 - Migrating to Vite]() video.
-- https://github.com/elibroftw/modern-desktop-app-template
-- https://github.com/carltonj2000/tauri-react-1
+```bash
+npm install
 
-For fs access add content below to `allowlist` of
+npm run dev # with cmd below for dev
+yarn tauri dev # with cmd above for dev
+
+npm run build
+npm run appimage
+```
+
+## Extra Features
+
+### FS Access
+
+For tauri fs access add content below to `allowlist` of
 `./src-tauri/tauri.confi.json`.
 
 ```json title="./src-tauri/tauri.confi.json"
@@ -14,6 +24,20 @@ For fs access add content below to `allowlist` of
         "scope": ["/*"]
       }
 ```
+
+### Local Storage
+
+```bash
+npm add localforage
+```
+
+## Creation History
+
+The code in this repository is base on the following:
+
+- [Tauri & ReactJS Part 5 - Migrating to Vite]() video.
+- https://github.com/elibroftw/modern-desktop-app-template
+- https://github.com/carltonj2000/tauri-react-1
 
 ```bash
 npm create vite@latest
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,70 @@
+import { useState, useEffect } from "react";
 import { writeFile, readDir } from "@tauri-apps/api/fs";
+import localforage from "localforage";
+
+function getItem(key, stateSetter, defaultValue) {
+  localforage
+    .getItem(key)
+    .then((value) => stateSetter(value))
+    .catch((_) => {
+      stateSetter(defaultValue);
+      localforage.setItem(key, defaultValue);
+    });
+}
 
 function App() {
+  const [colorScheme, setColorScheme] = useState("light");
+
+  const toggleColorScheme = (value) => {
+    const newValue = value || (colorScheme === "light" ? "dark" : "light");
+    setColorScheme(newValue);
+    localforage.setItem("colorScheme", newValue);
+  };
+
+  useEffect(() => {
+    getItem("colorScheme", setColorScheme, "light");
+  }, []);
+
   const onWriteFile = async () => {
-    console.log("file written");
     await writeFile({
       path: "/home/carltonj2000/tauri-react.txt",
       contents: "the file content",
     });
   };
+
   const onReadDir = async () => {
     const data = await readDir("/home/carltonj2000/");
     console.log("data read", data);
   };
+
   return (
-    <div className="App">
-      <button onClick={onWriteFile}>Write file</button>
-      <button onClick={onReadDir}>Read Dir</button>
-      <button onClick={() => console.log("hi")}>Write console</button>
+    <div>
+      <h1>Tauri, React And Vite Example</h1>
+
+      <div>
+        <h2>Text File Output</h2>
+        <button onClick={onWriteFile}>Write file</button>
+      </div>
+
+      <div>
+        <h2>Color Scheme Via Local Storage Using localforage</h2>
+        <button
+          style={{
+            border: "1px solid lightgray",
+            borderRadius: "5px",
+            outline: "none",
+            background: colorScheme === "dark" ? "#aaa" : "#eee",
+          }}
+          onClick={() => toggleColorScheme()}
+        >
+          Toggle Scheme
+        </button>
+      </div>
+
+      <div>
+        <h2>Debug Console Output From FS Read</h2>
+        <button onClick={onReadDir}>Read Dir</button>
+      </div>
     </div>
   );
 }
